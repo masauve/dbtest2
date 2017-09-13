f29ee89164e0f093299e482117a7a6549654176f
Martin Sauve <martin.sauve@gmail.com>
2017-09-13 16:23:31 -0400

    test

--- dbtest.php
+++ dbtest.php
@@ -4,21 +4,39 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER");
 $dbname = getenv("MYSQL_DATABASE");
 $dbpwd = getenv("MYSQL_PASSWORD");
-
-
-
+ 
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-
-
 $query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
+?>
 
-echo "Hello All!  Here is the list of users: <br>";
+<table border="1" cellspacing="2" cellpadding="2">
+<tr>
+<td>
+<font face="Arial, Helvetica, sans-serif">User ID</font>
+</td>
+<td>
+<font face="Arial, Helvetica, sans-serif">User Name</font>
+</td>
+</tr>
+
+<?php
 $rs = $connection->query($query);
 while ($row = mysqli_fetch_assoc($rs)) {
-    echo "User Id: ".$row['user_id'] . " User Name: " . $row['username'] . "<br>";
-}
-echo "End of the list <br>";
 
+?>
+
+<tr>
+<td>
+<font face="Arial, Helvetica, sans-serif"><?php echo $row['user_id']; ?></font>
+</td>
+<td>
+<font face="Arial, Helvetica, sans-serif"><?php echo $row['username']; ?></font>
+</td>
+</tr>
+
+
+<?php
+}
 mysqli_close($connection);
 
 ?>
