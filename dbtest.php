c73bd662a61511ebbd61f53401f8757b468bce17
Martin Sauve <martin.sauve@gmail.com>
2017-10-02 10:09:18 -0400

    bordure

--- dbtest.php
+++ dbtest.php
@@ -9,7 +9,7 @@ $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or d
 $query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
 ?>
 
-<table border="0" cellspacing="20" cellpadding="2">
+<table border="1" cellspacing="2" cellpadding="2">
 <tr>
 <td>
 <font face="Arial, Helvetica, sans-serif">User ID</font>
